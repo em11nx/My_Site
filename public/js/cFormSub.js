27a0f27c57bb36f688a4e8e9b438f9dbaa69feed
John Ippolito IV <johnippolitoiv@gmail.com>
2024-08-19 17:12:13 -0400

    Add a POST function to form to send to backend

--- public/js/cFormSub.js
+++ public/js/cFormSub.js
@@ -1,9 +1,33 @@
+const postButton = document.getElementById('CformSumbit')
+
+const baseURL = 'http://localhost:3000/'
+const formSubURL = 'http://localhost:3000/form/sinfo'
+
 var form = document.querySelector(".my-form");
-form.addEventListener("submit", function (e) {
+
+postButton.addEventListener('click', postInfo)
+async function postInfo(e) {
     e.preventDefault();
-    var formdata = new FormData(this);
-    var inputName = formdata.get("Name");
-    var inputEmail = formdata.get("Email");
-    var inputSubject = formdata.get("Subject");
+    var inputName = document.getElementById('CformName');
+    var inputEmail = document.getElementById('CformEmail');
+    var inputSubject = document.getElementById('CformSubject');
+    //            |
+    //change this\|/ to show in html
+    if(inputName.value === '' | inputEmail.value === '' | inputSubject.value === ''){console.log("Not all info filled out"); return}
     console.log(inputName, " ", inputEmail, " ", inputSubject);
-});
+    const res = await fetch(formSubURL,
+    {
+        
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            formContent: {
+                'name': inputName.value,
+                'email': inputEmail.value,
+                'subject': inputSubject.value
+            }
+        })
+    })
+}
--- server.js
+++ server.js
@@ -6,8 +6,17 @@ const app = express();
 const PORT = 3000;                 
 
 app.use(express.static('public'));
-app.use('/images', express.static('images'));
- 
+app.use(express.json())
+
+app.post('/form/sinfo', (req, res) => {
+  const { formContent } = req.body;
+  if(!formContent){
+    return res.status(400).send({status: 'failed'})
+  }
+  res.status(200).send({status: 'recived'});
+  console.log(formContent)
+})
+
 // Server setup
 app.listen(PORT, () => {
   console.log(`Running server on PORT ${PORT}...`);
