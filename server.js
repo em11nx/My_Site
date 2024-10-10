d3f430d31d5d9aca20add5bac0a3da4d0b894f6c
John Ippolito IV <johnippolitoiv@gmail.com>
2024-10-09 20:39:44 -0400

    Saves formData to a JSON file that doesn't format correctly

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const exp = require('constants');
 const express = require('express');
+const fs = require('fs')
 
 const app = express();
 
@@ -9,12 +10,21 @@ app.use(express.static('public'));
 app.use(express.json());
 
 app.post('/form/sinfo', (req, res) => {
+  const filePath = './data/formData.json'; 
   const { formContent } = req.body;
   if(!formContent){
     return res.status(400).send({status: 'failed'})
   }
-  res.status(200).send({status: 'recived'});
-  console.log(formContent)
+  const jsonString = JSON.stringify(formContent, null, 2);
+  fs.appendFile(filePath, jsonString, 'utf8', (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error saving data');
+    } else {
+      console.log('Data saved successfully');
+      res.status(200).send('Data saved');
+    }
+  });
 })
 
 // Server setup
